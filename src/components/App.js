d33034aa90ab0fb1c6637679b1b90e121ccf2603
Anja <73844750+tasteslikestrawberries@users.noreply.github.com>
2021-10-01 10:59:39 +0200

    added theme context component

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import { ThemeProvider } from '../contexts/ThemeContext'
 
 //components
 import Signup from './Signup';
+import TestComponent from './TestComponent';
 
 //styles
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container} from 'react-bootstrap'
 import { AuthProvider } from '../contexts/AuthContext';
 
+
 function App() {
   return (
   <>
+    <ThemeProvider>
+      <TestComponent />
+    </ThemeProvider>
+    
     <AuthProvider>
       <Container className='d-flex align-items-center justify-content-center' style={{minHeight:'100vh'}}>
         <div className='w-100' style={{maxWidth:'400px'}}>
